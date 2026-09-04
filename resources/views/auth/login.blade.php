c25fd26ff57f4cb2cdff107e6d886751407f897b
Yu-shn19 <shuwnednan.plazos@gmail.com>
2026-09-04 11:45:25 +0800

    logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="{{ $branding['org_short_name'] }} | Water Management System">
   <title>Login - {{ $branding['org_name'] }}</title>
-  <link rel="icon" href="{{ $branding['favicon_url'] }}">
+  <link rel="icon" type="image/png" href="{{ $branding['favicon_url'] }}">
+  <link rel="shortcut icon" type="image/png" href="{{ $branding['favicon_url'] }}">
+  <link rel="apple-touch-icon" href="{{ $branding['favicon_url'] }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
